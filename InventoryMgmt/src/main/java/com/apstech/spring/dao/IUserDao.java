fd07c101d3911c7531d44ce9c1098488a12e15fb
Sameer Ghimire <sameer.scrum2014@gmail.com>
2017-01-21 08:44:39 -0700

    Added methods to UserDao

--- InventoryMgmt/src/main/java/com/apstech/spring/dao/IUserDao.java
+++ InventoryMgmt/src/main/java/com/apstech/spring/dao/IUserDao.java
@@ -2,7 +2,7 @@ package com.apstech.spring.dao;
 
 import java.util.List;
 
-import com.apstech.models.UserDetails;
+import com.apstech.models.User;
 
 /**
  * Data access for users
@@ -17,26 +17,26 @@ public interface IUserDao {
 	 * @param user to be added
 	 * @return success or fail
 	 */
-	public boolean addUser(UserDetails user);
+	public boolean addUser(User user);
 	
 	/**
 	 * admin updates user record in the database
 	 * @param user to be updated
 	 * @return success or fail
 	 */
-	public boolean updateUser(UserDetails user);
+	public boolean updateUser(User user);
 	
 	/**
 	 * admin deletes the user from database
 	 * @param user to be deleted
 	 * @return success or fail
 	 */
-	public boolean delete(UserDetails user);
+	public boolean delete(User user);
 	
 	/**
 	 * Gets all users from the database
 	 * @return list of users
 	 */
-	public List<UserDetails> getAllUsers();
+	public List<User> getAllUsers();
 
 }
--- InventoryMgmt/src/main/java/com/apstech/spring/dao/UserDao.java
+++ InventoryMgmt/src/main/java/com/apstech/spring/dao/UserDao.java
@@ -1,33 +1,92 @@
 package com.apstech.spring.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
-import com.apstech.models.UserDetails;
+import javax.sql.DataSource;
+
+import com.apstech.models.User;
 
 public class UserDao implements IUserDao {
 
+	private static final String ADD_USER = "insert into users(firstname, lastname, username, password, email)"
+			+ "values(?,?,?,?,?)";
+	
+	private PreparedStatement preparedStatement;
+
+	private DataSource dataSource;
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	@Override
-	public boolean addUser(UserDetails user) {
+	public boolean addUser(User user) {
+		boolean result = false;
+		try {
+			PreparedStatement psmt = getPreparedStatement();
+			psmt.setString(1, user.getFirstName());
+			psmt.setString(2, user.getLastName());
+			psmt.setString(3, user.getUserName());
+			psmt.setString(4, user.getPassword());
+			psmt.setString(5, user.getEmail());
+			psmt.execute();
+			result = true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	private PreparedStatement getPreparedStatement() throws SQLException {
+		preparedStatement = dataSource.getConnection().prepareStatement(ADD_USER);
+		return preparedStatement;
+	}
+
+	@Override
+	public boolean updateUser(User user) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean updateUser(UserDetails user) {
+	public boolean delete(User user) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean delete(UserDetails user) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public List<User> getAllUsers() {
 
-	@Override
-	public List<UserDetails> getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> users = new ArrayList<User>();
+		User user = null;
+
+		try {
+			Connection connection = dataSource.getConnection();
+
+			String sql = "SELECT * from users";
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				user = new User();
+				user.setFirstName(rs.getString("firstName"));
+				user.setLastName(rs.getString("lastName"));
+				user.setUserName(rs.getString("userName"));
+				user.setPassword(rs.getString("password"));
+				user.setEmail(rs.getString("email"));
+				users.add(user);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return users;
+
 	}
 
 }
